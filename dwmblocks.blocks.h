7a179b51efb33b8ea680009641bfb5a87b863c17
Scott Cressi <scottcressi@gmail.com>
2020-09-01 22:26:23 -0400

    new path

--- dwmblocks.blocks.h
+++ dwmblocks.blocks.h
@@ -3,19 +3,19 @@ static const Block blocks[] = {
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
 // laptop
-{" 🔋: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_battery",1,0},
-{" 📶: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_strength",1,0},
-{" 📶: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_network",1,0},
+{" 🔋: ","bash ~/repos/personal/dwmblocks/status.sh status_battery",1,0},
+{" 📶: ","bash ~/repos/personal/dwmblocks/status.sh status_strength",1,0},
+{" 📶: ","bash ~/repos/personal/dwmblocks/status.sh status_network",1,0},
 
 // all
-{" VOL: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_volume",1,0},
-{" 🧠: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_memory",1,0},
-{" 💾: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_disk",1,0},
-{" 💻: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_cpu",1,0},
-{" CAPS: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_caps",1,0},
-{" 🌐: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_ip",1,0},
-{" ⏰: ","bash ~/repos/personal/suckless/dwmblocks/status.sh status_date",1,0},
-{"","bash ~/repos/personal/suckless/dwmblocks/status.sh status_wallpaper",300,0},
+{" VOL: ","bash ~/repos/personal/dwmblocks/status.sh status_volume",1,0},
+{" 🧠: ","bash ~/repos/personal/dwmblocks/status.sh status_memory",1,0},
+{" 💾: ","bash ~/repos/personal/dwmblocks/status.sh status_disk",1,0},
+{" 💻: ","bash ~/repos/personal/dwmblocks/status.sh status_cpu",1,0},
+{" CAPS: ","bash ~/repos/personal/dwmblocks/status.sh status_caps",1,0},
+{" 🌐: ","bash ~/repos/personal/dwmblocks/status.sh status_ip",1,0},
+{" ⏰: ","bash ~/repos/personal/dwmblocks/status.sh status_date",1,0},
+{"","bash ~/repos/personal/dwmblocks/status.sh status_wallpaper",300,0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
