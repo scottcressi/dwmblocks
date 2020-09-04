0a81361b6df8ebd005b298f9866b202cf1765242
Scott Cressi <scottcressi@gmail.com>
2020-09-04 17:36:44 -0400

    no utf8

--- dwmblocks.blocks.h
+++ dwmblocks.blocks.h
@@ -3,18 +3,18 @@ static const Block blocks[] = {
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
 // laptop
-{" 🔋: ","bash ~/repos/personal/dwmblocks/status.sh status_battery",1,0},
-{" 📶: ","bash ~/repos/personal/dwmblocks/status.sh status_strength",1,0},
-{" 📶: ","bash ~/repos/personal/dwmblocks/status.sh status_network",1,0},
+{" BAT: ","bash ~/repos/personal/dwmblocks/status.sh status_battery",1,0},
+{" SIG: ","bash ~/repos/personal/dwmblocks/status.sh status_strength",1,0},
+{" SSID: ","bash ~/repos/personal/dwmblocks/status.sh status_network",1,0},
 
 // all
 {" VOL: ","bash ~/repos/personal/dwmblocks/status.sh status_volume",1,0},
-{" 🧠: ","bash ~/repos/personal/dwmblocks/status.sh status_memory",1,0},
-{" 💾: ","bash ~/repos/personal/dwmblocks/status.sh status_disk",1,0},
-{" 💻: ","bash ~/repos/personal/dwmblocks/status.sh status_cpu",1,0},
-{" CAPS: ","bash ~/repos/personal/dwmblocks/status.sh status_caps",1,0},
-{" 🌐: ","bash ~/repos/personal/dwmblocks/status.sh status_ip",1,0},
-{" ⏰: ","bash ~/repos/personal/dwmblocks/status.sh status_date",1,0},
+{" MEM: ","bash ~/repos/personal/dwmblocks/status.sh status_memory",1,0},
+{" HDD: ","bash ~/repos/personal/dwmblocks/status.sh status_disk",1,0},
+{" CPU: ","bash ~/repos/personal/dwmblocks/status.sh status_cpu",1,0},
+{" CAP: ","bash ~/repos/personal/dwmblocks/status.sh status_caps",1,0},
+{" IP: ","bash ~/repos/personal/dwmblocks/status.sh status_ip",1,0},
+{" DATE: ","bash ~/repos/personal/dwmblocks/status.sh status_date",1,0},
 {"","bash ~/repos/personal/dwmblocks/status.sh status_wallpaper",300,0},
 };
 
