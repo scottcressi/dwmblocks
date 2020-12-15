3639f753e0497c7ad5c28d05e6f43db05a276a26
Scott Cressi <scottcressi@gmail.com>
2020-12-15 14:43:39 -0500

    remove spacing

--- dwmblocks.blocks.h
+++ dwmblocks.blocks.h
@@ -3,21 +3,21 @@ static const Block blocks[] = {
 /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
 // laptop
-{" BAT: ","bash ~/repos/personal/dwmblocks/status.sh status_battery",1,0},
-{" SIG: ","bash ~/repos/personal/dwmblocks/status.sh status_strength",1,0},
-{" SSID: ","bash ~/repos/personal/dwmblocks/status.sh status_network",1,0},
+{"BAT:","bash ~/repos/personal/dwmblocks/status.sh status_battery",1,0},
+{"SIG:","bash ~/repos/personal/dwmblocks/status.sh status_strength",1,0},
+{"SSID:","bash ~/repos/personal/dwmblocks/status.sh status_network",1,0},
 
 // all
-{" VOL: ","bash ~/repos/personal/dwmblocks/status.sh status_volume",1,0},
-{" MEM: ","bash ~/repos/personal/dwmblocks/status.sh status_memory",1,0},
-{" HDD: ","bash ~/repos/personal/dwmblocks/status.sh status_disk",1,0},
-{" CPU: ","bash ~/repos/personal/dwmblocks/status.sh status_cpu",1,0},
-{" CAP: ","bash ~/repos/personal/dwmblocks/status.sh status_caps",1,0},
-{" IP: ","bash ~/repos/personal/dwmblocks/status.sh status_ip",1,0},
-{" HOME: ","bash ~/repos/personal/dwmblocks/status.sh status_router",1,0},
-{" DATE: ","bash ~/repos/personal/dwmblocks/status.sh status_date",1,0},
+{"VOL:","bash ~/repos/personal/dwmblocks/status.sh status_volume",1,0},
+{"MEM:","bash ~/repos/personal/dwmblocks/status.sh status_memory",1,0},
+{"HDD:","bash ~/repos/personal/dwmblocks/status.sh status_disk",1,0},
+{"CPU:","bash ~/repos/personal/dwmblocks/status.sh status_cpu",1,0},
+{"CAP:","bash ~/repos/personal/dwmblocks/status.sh status_caps",1,0},
+{"IP:","bash ~/repos/personal/dwmblocks/status.sh status_ip",1,0},
+{"NET:","bash ~/repos/personal/dwmblocks/status.sh status_router",1,0},
+{"","bash ~/repos/personal/dwmblocks/status.sh status_date",1,0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
