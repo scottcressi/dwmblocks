375a695cb9f4bfd935f1a69b7e9eb97725c2ef51
Scott Cressi <scottcressi@gmail.com>
2021-04-19 14:02:54 -0400

    spaces

--- status.sh
+++ status.sh
@@ -2,18 +2,18 @@
 
 status_volume(){
     VOL=$(amixer sget Master | awk -F'[][]' '/Right:/ { print $2 }')
-    echo VOL: "$VOL"
+    echo VOL:"$VOL"
 }
 
 status_memory(){
     MEM=$(awk '/MemAvailable/ {printf( "%.1fg", $2 / 1024 / 1024 )}' /proc/meminfo)
-    echo MEM: "$MEM"
+    echo MEM:"$MEM"
 }
 
 status_battery(){
     if [ -d /sys/module/battery ] ; then
         BAT=$(cat /sys/class/power_supply/BAT1/capacity)
-        echo BAT: "$BAT"
+        echo BAT:"$BAT"
         if [ "$BAT" -lt 10 ] ; then
             notify-send --expire-time 4000 --urgency critical "$(date)" "BATTERY LOW"
         fi
@@ -24,59 +24,59 @@ status_strength(){
     if [ -d /sys/module/battery ] ; then
         INTERFACE="$(/sbin/iw dev | awk '$1=="Interface"{print $2}')"
         SIG=$(/sbin/iw dev "$INTERFACE" link | awk '/signal/ {print $2}')
-        echo SIG: "$SIG"
+        echo SIG:"$SIG"
     fi
 }
 
 status_network(){
     if [ -d /sys/module/battery ] ; then
         SSID=$(/sbin/iw dev | awk '/ssid/ {print $2}')
-        echo SSID: "$SSID"
+        echo SSID:"$SSID"
     fi
 }
 
 status_disk(){
     DISK=$(df -h / | awk '/dev/ {print $5}')
-    echo DISK: "$DISK"
+    echo DISK:"$DISK"
 }
 
 status_cpu(){
     CPU=$(awk '{print $1}' /proc/loadavg)
-    echo CPU: "$CPU"
+    echo CPU:"$CPU"
 }
 
 status_caps(){
     CAP=$(xset q | awk '/Caps/ {print $4}')
-    echo CAP: "$CAP"
+    echo CAP:"$CAP"
 }
 
 status_ip(){
     IP=$(ip route get 1.2.3.4 | awk '{print $7}')
-    echo IP: "$IP"
+    echo IP:"$IP"
 }
 
 status_router(){
     ROUTER=$(ip route | awk '/default/ {print $3}' | uniq)
     if [ "$(ping -c 1 "$ROUTER" -W 1 -q >/dev/null 2>&1 ; echo $?)" = "0" ] ; then
-        echo NET: up
+        echo NET:up
     else
-        echo NET: DOWN
+        echo NET:DOWN
         notify-send --expire-time 4000 --urgency critical "$(date)" "NETWORK DOWN"
     fi
 }
 
 status_date(){
-    date '+%a %b %-m/%d/%Y %_I:%M %S'
+    date '+%a %b %-m/%d/%Y%_I:%M %S'
 }
 
 status_containers(){
     CONTAINER=$(pgrep -c containerd-shim)
-    echo CONTAINER: "$CONTAINER"
+    echo CONTAINER:"$CONTAINER"
 }
 
 status_mounts(){
     MOUNT=$(mount  | grep -c 'cifs\|nfs')
-    echo MOUNT: "$MOUNT"
+    echo MOUNT:"$MOUNT"
 }
 
 $1
